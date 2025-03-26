9e06e1417fdb93f4c907f88d819f74d6eb2c7f06
psoethe <psoethe@gmail.com>
2025-03-26 15:35:29 -0300

    mudanças de adaptação

---
+++ java/script.js
@@ -0,0 +1,6 @@
+document.querySelector('.dropdown-btn').addEventListener('click', () => {
+    const menu = document.querySelector('.menu');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+  });
+
+  
